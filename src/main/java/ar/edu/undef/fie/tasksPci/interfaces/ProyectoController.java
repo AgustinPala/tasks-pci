205742afeda46ea43bc7c7841a8085b3312f325f
Agustin <ajpalacios@hotmail.com.ar>
2024-02-24 18:14:10 -0300

    ADD HATEOAS

--- src/main/java/ar/edu/undef/fie/tasksPci/interfaces/ProyectoController.java
+++ src/main/java/ar/edu/undef/fie/tasksPci/interfaces/ProyectoController.java
@@ -1,14 +1,21 @@
 package ar.edu.undef.fie.tasksPci.interfaces;
 
 import ar.edu.undef.fie.tasksPci.domain.entities.Proyecto;
+import ar.edu.undef.fie.tasksPci.domain.entities.Responsable;
 import ar.edu.undef.fie.tasksPci.domain.entities.Tarea;
 import ar.edu.undef.fie.tasksPci.infrastructure.ProyectoRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.hibernate.Hibernate.size;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("api/v1")
@@ -20,44 +27,56 @@ public class ProyectoController {
     }
 
     @GetMapping("proyectos")
-    public List<Proyecto> obtenerTodosLosProyectos() {
-        return repository.findAll();
+    public CollectionModel<EntityModel<Proyecto>> obtenerTodosLosProyectos() {
+        List<EntityModel<Proyecto>> proyectos = repository.findAll().stream()
+                .map(proyecto -> EntityModel.of(proyecto,
+                        linkTo(methodOn(ProyectoController.class).obtenerProyecto(proyecto.getId())).withSelfRel(),
+                        linkTo(methodOn(ProyectoController.class).obtenerTodosLosProyectos()).withRel("proyectos")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(proyectos, linkTo(methodOn(ProyectoController.class).obtenerTodosLosProyectos()).withSelfRel());
     }
 
     @PostMapping("proyectos")
-    private void crearProyecto(@RequestBody Proyecto proyecto) {
-        repository.save(proyecto);
+    public EntityModel<Proyecto> crearProyecto(@RequestBody Proyecto proyecto) {
+        Proyecto nuevoProyecto = repository.save(proyecto);
+        return EntityModel.of(nuevoProyecto,
+                linkTo(methodOn(ProyectoController.class).obtenerProyecto(nuevoProyecto.getId())).withSelfRel());
     }
 
     @GetMapping("proyectos/{id}")
-    private Optional<Proyecto> obtenerProyecto(@PathVariable Long id) {
-        return repository.findById(id);
+    public EntityModel<Proyecto> obtenerProyecto(@PathVariable Long id) {
+        Proyecto proyecto = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Proyecto no encontrado con id: " + id));
+        return EntityModel.of(proyecto,
+                linkTo(methodOn(ProyectoController.class).obtenerProyecto(id)).withSelfRel(),
+                linkTo(methodOn(ProyectoController.class).obtenerTodosLosProyectos()).withRel("proyectos"));
     }
 
     @PatchMapping("proyectos/{id}")
-    private Proyecto actualizarProyecto(@PathVariable Long id, @RequestBody Proyecto proyectoActualizado) {
-        return repository.findById(id)
-                .map(proyecto -> {
-                    if(proyectoActualizado.getNombre() != null) {
-                        proyecto.setNombre(proyectoActualizado.getNombre());
-                    }
-                    if(proyectoActualizado.getDescripcion() != null) {
-                        proyecto.setDescripcion(proyectoActualizado.getDescripcion());
-                    }
-                    if(proyectoActualizado.getTareas()!= null) {
-                        for (int i = 0; i < size(proyectoActualizado.getTareas()); i++) {
-                            proyecto.getTareas().add(proyectoActualizado.getTareas().get(i));
+    public EntityModel<Proyecto> actualizarProyecto(@PathVariable Long id, @RequestBody Proyecto proyectoActualizado) {
+        Proyecto proyecto = repository.findById(id)
+                .map(proyectoExistente -> {
+                    Optional.ofNullable(proyectoActualizado.getNombre()).ifPresent(proyectoExistente::setNombre);
+                    Optional.ofNullable(proyectoActualizado.getDescripcion()).ifPresent(proyectoExistente::setDescripcion);
+                    Optional.ofNullable(proyectoActualizado.getTareas()).ifPresent(tareas -> {
+                        for (Tarea tarea : tareas) {
+                            proyectoExistente.getTareas().add(tarea);
                         }
-                    }
-                    return repository.save(proyecto);
+                    });
+                    return repository.save(proyectoExistente);
                 })
                 .orElseThrow(() -> new RuntimeException("Proyecto no encontrado con id: " + id));
+        return EntityModel.of(proyecto,
+                linkTo(methodOn(ProyectoController.class).obtenerProyecto(id)).withSelfRel(),
+                linkTo(methodOn(ProyectoController.class).obtenerTodosLosProyectos()).withRel("proyectos"));
     }
 
     @DeleteMapping("proyectos/{id}")
-    private void eliminarProyecto(@PathVariable Long id) {
+    public ResponseEntity<String> eliminarProyecto(@PathVariable Long id) {
+        Proyecto proyecto = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Proyecto no encontrado con id: " + id));
         repository.deleteById(id);
+        return ResponseEntity.ok("Proyecto con id " + id + " eliminado correctamente.");
     }
-
-
 }
